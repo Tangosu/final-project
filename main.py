e246326f2da7345cdefcbfbef498dab6a0a43a93
DESKTOP-HK0SLHB\JT <jtang1200@gmail.com>
2022-03-15 12:05:24 +0000

    reading_patterns

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import cv2
 import detection
 
-capture = cv2.VideoCapture('assets/S10_Switronic_Short_Cut.mp4')
-# capture = cv2.VideoCapture('assets/S10_Switronic_Short_Cut_Test.mp4')
+v='assets/staircase.mp4'
+capture = cv2.VideoCapture(v)
 ret, frame1 = capture.read()
 
 # picture we are looking for
@@ -18,17 +18,22 @@ rl_axis = 70
 c_axis = 120
 rr_axis = 175
 br_axis = 230
+
+prev_co = []
+track_obj = {}
+unique_obj = []
+track_id = 0
+x = 490
+y = 320
+
 while True:
     ret, frame = capture.read()
 
     if ret == True:
+
+        current_co = []
         img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # bl works
-        # rl doesnt match for combo 43 / rl-c jump
-        # c works
-        # rr doesnt match for combo 44 / rr-c jump
-        # br
         # detection for arrows
         left_blue = detection.search(img, bl, bl_axis)
         left_red = detection.search(img, rl, rl_axis)
@@ -36,13 +41,72 @@ while True:
         right_red = detection.search(img, rr, rr_axis)
         right_blue = detection.search(img, br, br_axis)
 
-        cv2.imshow('PIU', img)
+        # adds the co-ordinates of the arrow to current array
+        if left_blue is not None:
+            current_co.append(left_blue)
+        if left_red is not None:
+            current_co.append(left_red)
+        if centre is not None:
+            current_co.append(centre)
+        if right_red is not None:
+            current_co.append(right_red)
+        if right_blue is not None:
+            current_co.append(right_blue)
 
-        if cv2.waitKey(0) == 27:
+        # Compares the current co-ordinate of the arrow to the previous co-ordinate.
+        # If the distance of the mid point is close (within 25 pxl), the current co-ordinate will be
+        # added to the track_obj dictionary. This is used as a way to track a unique moving object
+        if len(track_obj) < 1:
+            for i in range(len(prev_co)):
+                if len(current_co) == len(prev_co):
+                    dist = prev_co[i][1]-current_co[i][1]
+                    if dist < 25 and dist > 0:
+                        track_obj[track_id] = current_co
+                        track_id += 1
+
+        # Checks a copy of the track_obj dictionary and checks if the current co-ordinate is already in it.
+        else:
+            for j, k in track_obj.copy().items():
+                obj_exist = False
+                for i in current_co:
+                    dist = k[0][1] - i[1]
+                    if dist < 25 and dist > 0:
+                        track_obj[j] = [i]
+                        obj_exist = True
+                        continue
+                # If not, it will add it to the unique_obj array.
+                if not obj_exist:
+                    track_obj.pop(j)
+                    unique_obj.append(k)
+
+        prev_co = current_co.copy()
+        cv2.imshow('PIU', img)
+        if cv2.waitKey(1) == 27:
             break
     else:
         break
 
 capture.release()
 cv2.destroyAllWindows()
+
+# print(unique_obj)
+
+
+# Show the arrow sequence in an array
+new_arr = []
+for i in unique_obj:
+    if i[0][0] > 20 < 60:
+        new_arr.append("LB")
+    if i[0][0] > 70 < 110:
+        new_arr.append("LR")
+    if i[0][0] > 120 < 170:
+        new_arr.append("C")
+    if i[0][0] > 175 < 220:
+        new_arr.append("RR")
+    if i[0][0] > 230 < 270:
+        new_arr.append("RB")
+
+print(new_arr)
+
+
 
